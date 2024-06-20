3d994c567af91630b51a8a7fab83a5c03ca7bf33
Sheelah <sheelah.mogaka@gmail.com>
2024-06-20 10:20:02 +0000

    0-strace_is_your_friend.pp.

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,8 @@
- puppet manuscript to replace a line in a file on a server
+# A puppet manuscript to replace a line in a file on a server
 
 $file_to_edit = '/var/www/html/wp-settings.php'
 
-#replace line containing "phpp" with "php"
+# replace line containing "phpp" with "php"
 
 exec { 'replace_line':
   command => "sed -i 's/phpp/php/g' ${file_to_edit}",
