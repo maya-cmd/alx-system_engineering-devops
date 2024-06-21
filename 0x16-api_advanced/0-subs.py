3c7d6de248ea5027f483188472cde2bb49ad4320
Sheelah <sheelah.mogaka@gmail.com>
2024-06-21 05:06:48 -0700

    0-subs.py.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,13 +9,11 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "Mozilla/5.0 (compatible; MyRedditScript/1.0; +http://www.example.com)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
-        print("OK")
         return subscribers
     else:
-        print("OK")
         return 0
